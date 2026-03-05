f06185c4f1b0a5e721b7028e417f4b3d9542fb09
dhiraj kormocha <dhirajkormocha@dhirajs-MacBook-Pro.local>
2026-03-05 12:35:30 +0000

    setting up security

---
+++ app/security.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    
+    user = 'placeholder'
+
+    return user
+

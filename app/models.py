62f167b197426d5641d85ff1fcb1d157c90521d1
dhiraj kormocha <dhirajkormocha@dhirajs-MacBook-Pro.local>
2026-03-05 12:27:10 +0000

    change 'name' to 'username'

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
+    username = Column(String, nullable=False)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String, nullable=False)
     first_name =  Column(String, nullable=False)
